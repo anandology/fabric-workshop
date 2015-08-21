48e3d09ede8768eb056fbd0b0874a93668d0e1ec
Anand Chitipothu <anandology@gmail.com>
2015-08-22 00:11:13 +0530

    Added more tasks

--- fabfile.py
+++ fabfile.py
@@ -5,17 +5,30 @@ env.password = "vagrant"
 env.hosts = ["192.168.50.10", "192.168.50.11"]
 
 @task
-def host_type():
-    run('uname -s')
+def hostname():
+    """Prints hostname
+    """
+    run('hostname')
 
 @task
-def helloworld():
+def hello():
+    """Prints hello world
+    """
     run("echo hello world")
 
 @task
 def root():
+    """Runs something as root"""
     sudo("whoami")
 
 @task
 def sleep():
+    """Sleeps for 5 seconds"""
     run("sleep 5")
+
+@task
+def createdb(name):
+    """Task to create a database on all nodes.
+    """
+    # creates a database with given name on all the nodes
+    run("createdb " + name)
