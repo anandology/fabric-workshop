f4add87fb6f2f7f125a8877f929b945b6debe81d
Anand Chitipothu <anandology@gmail.com>
2015-08-22 11:00:51 +0530

    more tasks

--- fabfile.py
+++ fabfile.py
@@ -1,14 +1,23 @@
-from fabric.api import env, task, run, sudo
+from fabric.api import env, task, run, sudo, cd, local, get
 
 env.user = "vagrant"
 env.password = "vagrant"
-env.hosts = ["192.168.50.10", "192.168.50.11"]
+#env.hosts = ["192.168.50.10", "192.168.50.11"]
+
+@task
+def even():
+    env.hosts = ["192.168.50.10"]
+
+@task
+def odd():
+    env.hosts = ["192.168.50.11"]
 
 @task
 def hostname():
     """Prints hostname
     """
-    run('hostname')
+    print "running hostname task"
+    repr(run('hostname'))
 
 @task
 def hello():
@@ -27,8 +36,27 @@ def sleep():
     run("sleep 5")
 
 @task
+def touch(filename="/tmp/a.txt"):
+    """touchs a file on all nodes."""
+    run("touch " + filename)
+
+@task
 def createdb(name):
     """Task to create a database on all nodes.
     """
     # creates a database with given name on all the nodes
     run("createdb " + name)
+
+
+@task
+def backupdb():
+    run("mkdir -p /tmp/backups")
+    with cd("/tmp/backups"):
+        run("pg_dumpall | gzip > db-backup.gz")
+
+@task
+def sync_backups():
+    local("mkdir -p backups/")
+    get("/tmp/backups/db-backup.gz", "backups/%(host)s-db-backup.gz")
+    #get("/tmp/backups", "backups/%(host)s/")
+    #run("scp /tmp/backups/db-backup.gz backupserver:/tmp/as")
